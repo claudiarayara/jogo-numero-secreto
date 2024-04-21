e86b7b8e5b7c6d36b1ba61d032f7030eb10892e7
Claudia Rayara <claudia_rayara@hotmail.com>
2024-04-20 22:26:04 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,80 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do Número Secreto';
+
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+//Refatorar o código para não ficar repetitivo:
+//Criar uma função que vai exibir os textos na tela;
+//Obs.: Entre parenteses estão os parâmetros que eu quero que a função busque e traga. No caso, tudo o que esta em tag e texto;
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+}
+
+//Nas linhas abaixo estamos chamando a função exibirTextoNaTela e passando qual a "tag" e "texto" que queremos que seja exibido respetivamente;
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparInput();
+    }
+}
+
+//a função "includes" irá verificar para nós se temos na nossa lista o número sorteado
+//caso não: o "push" irá incluir o número na lista
+//caso sim: irá ser sortear novo número e refazer a verificação; 
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
+}
+
+//Um campo vazio entre aspas '' siginifica que eu quero uma string (texto) em branco;
+function limparInput() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparInput();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
